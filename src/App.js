3d3bf676290d445469f917b02866170d2ea39bf3
Prakhar Gupta <prakgup20@gmail.com>
2023-04-29 10:39:08 +0530

    add border annd some styling

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import ToolTip from "./toolTip";
 function App() {
   const [pos, setPos] = useState("right");
   const style = {
-    height: "40vh",
+    height: "35vh",
+    width:"40vw",
+    margin:"auto",
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
+    border: "5px dashed black",
+    padding:"10px",
   };
 
   return (
     <div className="App">
+      <h1>Tool-Tip Using React</h1>
+       <h3>Click on buttons to position tool-tip accordingly.</h3>
       <div className="container" style={style}>
         <button onClick={() => setPos("top")}>Top</button>
 
         <div
           style={{
-            width: "60%",
+            width: "100%",
             display: "flex",
             justifyContent: "space-between",
           }}
